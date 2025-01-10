db1c107ccf3ecfcaee5ec89d0736a42a1c3d8cd6
simonegarofalo <simonegarofalo96@gmail.com>
2025-01-10 12:24:57 +0100

    Installed Flowbite and realised hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}',
+  "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
